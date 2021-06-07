141142f7717811284b34c1d998b575b75980f1c6
Serega <Schuganiserega@mail.ru>
2021-06-07 19:50:00 +0400

    Size constructor and index operatros DONE

---
+++ List/main.cpp
@@ -0,0 +1,338 @@
+﻿#include<iostream>
+using namespace std;
+using std::cout;
+using std::endl;
+using std::cin;
+
+#define tab "\t"
+#define delimiter "\t-------------------------------------------------------\t"
+
+class List
+{
+	class Element
+	{
+		int Data;      //Значение элемента
+		Element* pNext;//указатель на следующий элемент
+		Element* pPrev;//указатель на предыдущий элемент
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr)
+		{
+			this->Data = Data;
+			this->pNext = pNext;
+			this->pPrev = pPrev;
+#ifdef DEBUG
+			cout << "EConstructor:\t" << this << endl;
+#endif // DEBUG
+		}
+		~Element()
+		{
+#ifdef DEBUG
+			cout << "EDestructor:\t" << this << endl;
+#endif // DEBUG
+		}
+		friend class List;
+	};
+	Element* Head;
+	Element* Tail;
+	unsigned int size;
+public:
+	class Iterator 
+	{
+		Element* Temp;
+	public:
+		Iterator(Element* Temp = nullptr) :Temp(Temp)
+		{
+#ifdef DEBUG
+			cout << "ITConstructor:\t" << this << endl;
+#endif // DEBUG
+		}
+		~Iterator()
+		{
+#ifdef DEBUG
+			cout << "ITDestructor:\t" << this << endl;
+#endif // DEBUG
+		}
+
+		Iterator& operator++()
+		{
+			Temp = Temp->pNext;
+			return* this;
+		}
+
+		Iterator operator++(int)
+		{
+			Iterator old = *this;
+			Temp = Temp->pNext;
+			return old;
+		}
+
+		bool operator==(const Iterator& other)const
+		{
+			return this->Temp == other.Temp;
+		}
+		bool operator!=(const Iterator& other)const
+		{
+			return this->Temp != other.Temp;
+		}
+
+		const int& operator*()const
+		{
+			return Temp->Data;
+		}
+		int& operator*()
+		{
+			return Temp->Data;
+		}
+
+	};
+	size_t getSize()const
+	{
+		return size;
+	}
+
+	Iterator begin()
+	{
+		return Head;
+	}
+	Iterator end()
+	{
+		return nullptr;
+	}
+
+	List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	explicit List(int size) :List()
+	{
+		while (size--)puch_back(0);
+	}
+	List(const initializer_list<int>& il) :List()
+	{
+		cout << typeid(il.begin()).name() << endl;
+		for (int const* it = il.begin(); it != il.end(); it++)puch_back(*it);
+	}
+	~List()
+	{
+		//while (Head)pop_front();
+		while (Tail)pop_back();
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//               Operators:
+	int& operator[](int index)
+	{
+		Element* Temp;
+		if (index < size / 2)
+		{
+			Temp = Head;
+			for (size_t i = 0; i < index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (size_t i = 0; i < size - index - 1; i++)Temp = Temp->pPrev;
+		}
+		return Temp->Data;
+	}
+
+	//               Adding elements:
+	void puch_front(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+			size++;
+			return;
+		}
+		/*Element* New = new Element(Data);
+		New->pNext = Head;
+		Head->pPrev = New;
+		Head = New;*/
+		Head = Head->pPrev = new Element(Data, Head);
+		size++;
+	}
+	void puch_back(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+			size++;
+			return;
+		}
+		/*Element* New = new Element(Data);
+		New->pPrev = Tail;
+		Tail->pNext = New;
+		Tail = New;*/
+		Tail = Tail->pNext = new Element(Data, nullptr, Tail);
+		size++;
+	}
+	void insert(unsigned int Index, int Data)
+	{
+		if (Index > size)return;
+		if (Index == 0)
+		{
+			puch_front(Data);
+			return;
+		}
+		if (Index == size)
+		{
+			puch_back(Data);
+			return;
+		}
+		Element* Temp;
+		if (Index < size / 2)
+		{
+			Temp = Head;
+			for (size_t i = 0; i < Index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (size_t i = 0; i < size - Index - 1; i++)Temp = Temp->pPrev;
+		}
+		/*Element* New = new Element(Data);
+		New->pNext = Temp;
+		New->pPrev = Temp->pPrev;
+		Temp->pPrev->pNext = New;
+		Temp->pPrev = New;*/
+		Temp->pPrev = Temp->pPrev->pNext = new Element(Data, Temp, Temp->pPrev);
+		size++;
+	}
+
+	//            Erasing elements:
+	void pop_front()
+	{
+		if (Head == Tail)
+		{
+			delete Head;
+			Head = Tail = nullptr;
+			size--;
+			return;
+		}
+		Head = Head->pNext;
+		delete Head->pPrev;
+		Head->pPrev = nullptr;
+		size--;
+	}
+
+	void pop_back()
+	{
+		if (Head == Tail)
+		{
+			delete Tail;
+			Head = Tail = nullptr;
+			size--;
+			return;
+		}
+		Tail = Tail->pPrev;
+		delete Tail->pNext;
+		Tail->pNext = nullptr;
+		size--;
+	}
+	void erase(int index)
+	{
+		if (index == 0)
+		{
+			pop_front();
+			return;
+		}
+		if (index == size-1)
+		{
+			pop_back();
+			return;
+		}
+		if (index >= size)return;
+
+		Element* Temp;
+		if (index < size / 2)
+		{
+			Temp = Head;
+			for (size_t i = 0; i < index; i++)Temp = Temp->pNext;
+		}
+		else
+		{
+			Temp = Tail;
+			for (size_t i = 0; i < size - index - 1; i++)Temp = Temp->pPrev;
+		}
+		//1)Исключаем удаляемый элемент из списка:
+		Temp->pPrev->pNext = Temp->pNext;
+		Temp->pNext->pPrev = Temp->pPrev;
+		//2) Удаляем элемент из памяти:
+		delete Temp;
+	}
+
+	//            Methods:
+	void print()
+	{
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+		{
+			cout << Temp << tab << Temp->pPrev << tab << Temp->Data << tab << Temp->pNext << endl;
+		}
+		cout << "Количество элементов списка: \t" << size << endl;
+	}
+	void print_reverse()
+	{
+		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+			cout << Temp << tab << Temp->pPrev << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Количество элементов списка: \t" << size << endl;
+	}
+};
+
+//#define BASE_CHECK
+//#define SIZE_CONSTRUCTOR_AND_INDEX_OPERATOR
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+#ifdef BASE_CHECK
+	List list;
+	for (int i = 0; i < n; i++)
+		list.puch_back(rand()%100);
+	/*list.print();
+	list.pop_back();*/
+	list.print();
+	cout << delimiter << endl;
+	list.print_reverse();
+	int index;
+	int value;
+	cout << delimiter << endl;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(index, value);
+	cout << delimiter << endl;
+	list.print();
+	cout << delimiter << endl;
+	list.print_reverse();
+	cout << delimiter << endl;
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	list.erase(index);
+	list.print();
+	cout << delimiter << endl;
+	list.print_reverse();
+	cout << delimiter << endl;
+#endif // BASE_CHECK
+
+#ifdef SIZE_CONSTRUCTOR_AND_INDEX_OPERATOR
+	List list(n);
+	for (size_t i = 0; i < list.getSize(); i++)list[i] = rand()%100;
+	for (size_t i = 0; i < list.getSize(); i++)cout << list[i] << tab;
+	cout << endl;
+	list.print();
+#endif // SIZE_CONSTRUCTOR_AND_INDEX_OPERATOR
+
+	List list = { 3, 5, 8, 13 ,21 };
+	list.print();
+	for (int i : list)
+		cout << i << tab;
+	cout << endl;
+	for (List::Iterator it = list.begin(); it != list.end(); it++)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+}
